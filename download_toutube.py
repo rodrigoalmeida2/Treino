d158cdad69cacb2211f0fd14e9fd747ad716fb06
rodrigoalmeida2 <rodrigoalmeida350.ra@gmail.com>
2024-10-12 16:57:34 -0300

    Donwload youtube videos

---
+++ download_toutube.py
@@ -0,0 +1,19 @@
+import yt_dlp # type: ignore
+
+# Função para baixar o vídeo
+def download_video(url, output_filename="video.mp4"):
+    ydl_opts = {
+        'format': 'best',  # Escolhe o formato de melhor qualidade
+        'outtmpl': output_filename  # Nome do arquivo de saída
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+# URL do vídeo que você quer baixar
+video_url = "https://youtu.be/2QcSvOSXTBc"
+
+# Baixar o vídeo
+download_video(video_url)
+
+print(f"Download concluído. O vídeo foi salvo como 'video.mp4'")
