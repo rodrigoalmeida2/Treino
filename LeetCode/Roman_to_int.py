06be2fc36374a7a5ba790d78aaf546e51e9fdc8a
rodrigoalmeida2 <rodrigoalmeida350.ra@gmail.com>
2025-03-30 22:20:10 -0300

    Romsn to int

---
+++ LeetCode/Roman_to_int.py
@@ -0,0 +1,36 @@
+def roman_to_int(s: str) -> int:
+    """
+    Converts a Roman numeral to an integer.
+    
+    :param s: Roman numeral string (e.g., "IX", "LVIII", "MCMXCIV")
+    :return: Integer value of the Roman numeral
+    """
+    roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    for char in reversed(s):
+        current_value = roman_map[char]
+        if current_value < prev_value:
+            total -= current_value
+        else:
+            total += current_value
+        prev_value = current_value
+    
+    return total
+
+# Test cases
+test_cases = ["III", "IV", "IX", "LVIII", "MCMXCIV", "MMXXIII"]
+x = map(roman_to_int, test_cases)
+result = list(x)
+for i, roman in enumerate(test_cases):
+    print(f"{roman} = {result[i]}")
